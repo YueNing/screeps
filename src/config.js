7fa99c37e9ec5cccd1b3d299bc308e759b1d11d4
naodongbanana <n1085633848@outlook.com>
2021-04-07 02:00:28 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -59,10 +59,10 @@ global.config = {
 
   // use username `tooangels` and password `tooSecretPassword` at https://screepspl.us/grafana
   stats: {
-    screepsPlusEnabled: true,
+    screepsPlusEnabled: false,
     screepsPlusToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRvb2FuZ2VscyIsImlhdCI6MTQ4MzU2MTU3OSwiYXVkIjoic2NyZWVwc3BsLnVzIiwiaXNzIjoic2NyZWVwc3BsLnVzIn0.NhobT7Jg8bOAg-MYqrYsgeMgXEVXGVYG9s3G9Qpfm-o',
-    enabled: true,
-    summary: true,
+    enabled: false,
+    summary: false,
   },
 
   debug: {
